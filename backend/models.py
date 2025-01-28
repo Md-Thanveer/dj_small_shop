ed618ef13218a44e62886eef561c2ff1f9703d4d
Md-Thanveer <thanveerami612@gmail.com>
2025-01-28 19:19:52 +0530

    Create the Brand Models

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Category(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -9,3 +10,23 @@ class Category(models.Model):
         return self.name
     class Meta:
         db_table = "category"
+
+class BrandStatus(models.TextChoices):
+    PUBLISHED = 'PUBLISHED', _('PUBLISHED')
+    DRAFT = 'DRAFT', _('DRAFT')
+    PENDING = 'PENDING', _('PENDING')
+
+class Brand(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name= models.CharField(max_length=255)
+    image_path= models.ImageField(upload_to='brands', null=True, blank=True, default='No_image_available.jpg')
+    status= models.CharField(
+        max_length=255,
+        choices=BrandStatus.choices,
+        default=BrandStatus.PUBLISHED
+    )
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        db_table = 'brand'
