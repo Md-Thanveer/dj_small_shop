ec503abc43a2ad5650c03e68cd2652ac005261df
Md-Thanveer <thanveerami612@gmail.com>
2025-02-06 12:02:56 +0530

    Create CartViewSet route added

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -2,13 +2,14 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet,BrandViewSet,ProductViewSet
+from .views import CategoryViewSet,BrandViewSet,ProductViewSet,CartViewSet
 
 router = DefaultRouter()
 
 router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'brands', BrandViewSet, basename='brand')
 router.register(r'products',ProductViewSet,basename='product')
+router.register(r'carts',CartViewSet,basename='cart')
 
 
 urlpatterns = [
