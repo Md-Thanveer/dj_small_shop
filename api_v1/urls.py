d44797ba902e960df725f865cc53b2734a26a671
Md-Thanveer <thanveerami612@gmail.com>
2025-02-06 11:47:46 +0530

    ProductViewSet Route added

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet,BrandViewSet
+from .views import CategoryViewSet,BrandViewSet,ProductViewSet
 
 router = DefaultRouter()
 
 router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'brands', BrandViewSet, basename='brand')
+router.register(r'products',ProductViewSet,basename='product')
 
 
 urlpatterns = [
