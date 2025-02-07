fc90a682c754e047547cd1eb85e26e2fb9b18f9c
Md-Thanveer <thanveerami612@gmail.com>
2025-02-07 20:03:00 +0530

    added the OrderViewSet in urls.py

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet,BrandViewSet,ProductViewSet,CartViewSet
+from .views import CategoryViewSet,BrandViewSet,ProductViewSet,CartViewSet,OrderViewSet
 
 router = DefaultRouter()
 
@@ -10,6 +10,7 @@ router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'brands', BrandViewSet, basename='brand')
 router.register(r'products',ProductViewSet,basename='product')
 router.register(r'carts',CartViewSet,basename='cart')
+router.register(r'orders',CartViewSet,basename='order')
 
 
 urlpatterns = [
